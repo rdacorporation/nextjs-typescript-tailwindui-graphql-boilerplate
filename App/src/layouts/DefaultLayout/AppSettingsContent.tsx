3d8c4ba234b1d3792dccae208df54449a5c195a6
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-02-18 01:42:53 -0500

    Remove additional artifacts/make generic

--- App/src/layouts/DefaultLayout/AppSettingsContent.tsx
+++ App/src/layouts/DefaultLayout/AppSettingsContent.tsx
@@ -3,8 +3,8 @@ import { observer } from 'mobx-react';
 const AppSettingsContent = () => {
   return (
     <div className="absolute inset-0 px-4 sm:px-6">
-      <a target="_blank" href="/api/getServerCertificate">
-        <button className="btn btn-primary">Download Device Certificate</button>
+      <a target="_blank" href="/api/test123">
+        <button className="btn btn-primary">Some Setting</button>
       </a>
     </div>
   );
