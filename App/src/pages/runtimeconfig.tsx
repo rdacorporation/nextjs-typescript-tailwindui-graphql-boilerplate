03f079ac4981dc6b236675ff51d8b7442485a873
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-02-23 12:18:54 -0500

    Flesh out demo

--- App/src/pages/runtimeconfig.tsx
+++ App/src/pages/runtimeconfig.tsx
@@ -1,19 +1,34 @@
 import getConfig from 'next/config';
+import { NextPageContext } from 'next';
 
 import DefaultLayout from '@layouts/DefaultLayout';
+interface EnvPageProps {
+  message: string;
+}
+function EnvPage({ message }: EnvPageProps) {
+  // Only holds serverRuntimeConfig and publicRuntimeConfig
+  const { publicRuntimeConfig } = getConfig();
 
-// Only holds serverRuntimeConfig and publicRuntimeConfig
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-// Will only be available on the server-side
-console.log(serverRuntimeConfig.HELLO_WORLD);
-// Will be available on both server-side and client-side
-console.log(publicRuntimeConfig.PUBLIC_HELLO_WORLD);
-
-export default function EnvPage() {
   return (
     <DefaultLayout>
       <h1>Runtime Environment Variables:</h1>
-      <p>HELLO_WORLD: {publicRuntimeConfig.PUBLIC_HELLO_WORLD}</p>
+      <p>HELLO_WORLD: {message}</p>
+      <p>PUBLIC_HELLO_WORLD: {publicRuntimeConfig.PUBLIC_HELLO_WORLD}</p>
     </DefaultLayout>
   );
 }
+
+EnvPage.getInitialProps = async (_: NextPageContext): Promise<EnvPageProps> => {
+  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+  // Will only be available on the server-side
+  console.log(serverRuntimeConfig.HELLO_WORLD);
+
+  // Will be available on both server-side and client-side
+  console.log(publicRuntimeConfig.PUBLIC_HELLO_WORLD);
+
+  // Exporting the server runtime config as a prop will make it available via the prop
+  return { message: serverRuntimeConfig.HELLO_WORLD };
+};
+
+export default EnvPage;
